3b72c4ecc89f482d4d36c86709a398da5d5e10a9
nabla27 <nabla3.1415@gmail.com>
2022-04-28 19:45:48 +0900

    move the drag event code of the table to source code.

--- gnuplottable.h
+++ gnuplottable.h
@@ -28,58 +28,9 @@ public slots:
     void setGnuplot(Gnuplot *gnuplot) { this->gnuplot = gnuplot; }
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override
-    {
-        const int cursorX = event->pos().x();
-        const int cursorY = event->pos().y();
-
-        const int rowLastIndex = rowCount() - 1;
-        const int colLastIndex = columnCount() - 1;
-
-        /* 一番右下のセルの右下隅の座標(tableWidgetのローカル座標) */
-        const int tableWidth = columnViewportPosition(colLastIndex) + columnWidth(colLastIndex);
-        const int tableHeight = rowViewportPosition(rowLastIndex) + rowHeight(rowLastIndex);
-
-        if(std::abs(cursorX - tableWidth) < 4 && std::abs(cursorY - tableHeight) < 4)
-        {
-            startDragPoint = event->pos();
-            startDragTableSize = QPoint(columnCount(), rowCount());
-            startDragCellSize = QPoint(columnWidth(colLastIndex), rowHeight(rowLastIndex));
-        }
-
-        TableWidget::mousePressEvent(event);
-    }
-    void mouseMoveEvent(QMouseEvent *event) override
-    {
-        if(startDragPoint == QPoint(-1, -1)) return;
-
-        setCursor(Qt::ClosedHandCursor);
-
-        /* 追加・削除分のセル数 */
-        const int dx = (event->pos().x() - startDragPoint.x()) / startDragCellSize.x();
-        const int dy = (event->pos().y() - startDragPoint.y()) / startDragCellSize.y();
-
-        /* 変更後のセル数 */
-        const int nextCol = startDragTableSize.x() + dx;
-        const int nextRow = startDragTableSize.y() + dy;
-
-        /* セルが1x1以下に減らないようにする */
-        if(nextCol > 0)
-            setColumnCount(startDragTableSize.x() + dx);
-        if(nextRow > 0)
-        setRowCount(startDragTableSize.y() + dy);
-
-        TableWidget::mouseMoveEvent(event);
-    }
-    void mouseReleaseEvent(QMouseEvent *event) override
-    {
-        startDragPoint = QPoint(-1, -1);
-
-        TableWidget::mouseReleaseEvent(event);
-        setCursor(Qt::ArrowCursor);
-    }
-
-
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 private slots:
     void onCustomContextMenu(const QPoint& point);
