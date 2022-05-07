4be3e01f2df4a5335a7995aed3e09c3c080a3bfa
nabla27 <nabla3.1415@gmail.com>
2022-05-07 16:09:06 +0900

    enable to download zip file and unzip the file.

--- GnuplotEditor.pro
+++ GnuplotEditor.pro
@@ -1,4 +1,5 @@
 QT       += core gui \
+            network widgets
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -12,40 +13,16 @@ SOURCES += \
     ../LabQHome/layoutparts.cpp \
     ../LabQHome/tablewidget.cpp \
     ../LabQHome/utility.cpp \
-    #browserwidget.cpp \
-    #consolewidget.cpp \
-    #editorsettingwidget.cpp \
-    #editorsyntaxhighlighter.cpp \
-    #filetree.cpp \
-    #gnuplot.cpp \
-    #gnuplotcpl.cpp \
-    #gnuploteditor.cpp \
-    #gnuplottable.cpp \
-    #main.cpp \
-    #menubar.cpp \
-    #texteditor.cpp \
-    #windowmenubar.cpp
 
 HEADERS += \
     ../LabQHome/iofile.h \
     ../LabQHome/layoutparts.h \
     ../LabQHome/tablewidget.h \
     ../LabQHome/utility.h \
-    #browserwidget.h \
-    #consolewidget.h \
-    #editorsettingwidget.h \
-    #editorsyntaxhighlighter.h \
-    #filetree.h \
-    #gnuplot.h \
-    #gnuplotcpl.h \
-    #gnuploteditor.h \
-    #gnuplottable.h \
-    #menubar.h \
-    #texteditor.h \
-    #windowmenubar.h
 
 INCLUDEPATH += E:/boost_1_76_0/boost_1_76_0 \
                E:/repos/qt_project/LabQ/LabQHome \
+               E:/zipper \
 
 include(src/src.pri)
 
@@ -53,3 +30,5 @@ include(src/src.pri)
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+
--- src/updatemanager.cpp
+++ src/updatemanager.cpp
@@ -2,41 +2,222 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QToolButton>
+#include <QDialogButtonBox>
 
+#include <QFileDialog>
+
+#include <QtWidgets>
+#include <QtNetwork>
+
+#include "zipper/unzipper.h"
+
+
+const QString UpdateManager::defaultFileName = "pltEditor.zip";
 const QString UpdateManager::downloadUrl = "https://github.com/nabla27/GnuplotEditor/archive/refs/heads/master.zip";
+
+
+ProgressDialog::ProgressDialog(const QUrl& url, QWidget *parent)
+    : QProgressDialog(parent)
+{
+    setWindowTitle("Download Progress");
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setLabelText(QString("Downloading %1.").arg(url.toDisplayString()));
+    setMinimum(0);
+    setValue(0);
+    setMinimumDuration(0);
+    setMinimumSize(QSize(400, 76));
+}
+
+void ProgressDialog::replyProgress(qint64 bytesRead, qint64 totalBytes)
+{
+    if(totalBytes == -1)
+        setMaximum(20000000);
+    else
+        setMaximum(totalBytes);
+
+    setValue(bytesRead);
+}
+
+
 
 UpdateManager::UpdateManager(QWidget *parent)
     : QDialog(parent)
     , urlLineEdit(new QLineEdit(downloadUrl, this))
+    , directoryLineEdit(new QLineEdit("", this))
     , versionLineEdit(new QLineEdit("", this))
     , messageLabel(new QLabel("", this))
+    , updateButton(new QPushButton("Update", this))
+    , quitButton(new QPushButton("Quit", this))
+    , unzipProcess(new QProcess(this))
 {
+    setMinimumWidth(500);
+
     QVBoxLayout *vLayout = new QVBoxLayout(this);
     QHBoxLayout *urlLayout = new QHBoxLayout;
+    QHBoxLayout *directoryLayout = new QHBoxLayout;
     QHBoxLayout *versionLayout = new QHBoxLayout;
     QSpacerItem *spacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
     setLayout(vLayout);
     vLayout->addLayout(urlLayout);
+    vLayout->addLayout(directoryLayout);
     vLayout->addLayout(versionLayout);
+    vLayout->addWidget(messageLabel);
     vLayout->addItem(spacer);
 
-    QLabel *urlLabel = new QLabel("Url", this);
+    QLabel *urlLabel = new QLabel("URL", this);
+    QLabel *directoryLabel = new QLabel("Directory", this);
+    QToolButton *selectDirButton = new QToolButton(this);
     QLabel *versionLabel = new QLabel("Version", this);
-
     urlLayout->addWidget(urlLabel);
     urlLayout->addWidget(urlLineEdit);
+    directoryLayout->addWidget(directoryLabel);
+    directoryLayout->addWidget(directoryLineEdit);
+    directoryLayout->addWidget(selectDirButton);
     versionLayout->addWidget(versionLabel);
     versionLayout->addWidget(versionLineEdit);
+    connect(selectDirButton, &QToolButton::released, this, &UpdateManager::selectDirectory);
 
-    const int label_width = 100;
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(this);
+    vLayout->addWidget(buttonBox);
+    buttonBox->addButton(updateButton, QDialogButtonBox::ButtonRole::ActionRole);
+    buttonBox->addButton(quitButton, QDialogButtonBox::ButtonRole::RejectRole);
+    connect(updateButton, &QPushButton::released, this, &UpdateManager::requestUpdate);
+    connect(quitButton, &QPushButton::released, this, &UpdateManager::close);
+
+    const int label_width = 60;
     urlLabel->setFixedWidth(label_width);
+    directoryLabel->setFixedWidth(label_width);
     versionLabel->setFixedWidth(label_width);
 
     urlLineEdit->setReadOnly(true);
     versionLineEdit->setReadOnly(true);
+    selectDirButton->setText("...");
+
+    /* カレントディレクトリからさかのぼって、binフォルダのあるパス(=アプリケーションのパス)を特定する */
+    const QString currentDirectory = QDir::currentPath();
+    const qsizetype sp = currentDirectory.count('/');
+    for(qsizetype i = 0; i < sp; ++i)
+    {
+        const QString dir = currentDirectory.section('/', 0, -i); //'/'でセクションに区切る。一番左0から右からのセクションi番目を取り出す
+        if(QDir(dir).entryList().contains("bin"))
+        {
+            oldFolderPath = dir;
+            break;
+        }
+    }
+
+    directoryLineEdit->setText(oldFolderPath.first(oldFolderPath.lastIndexOf('/')));
 }
 
 UpdateManager::~UpdateManager()
 {
 
 }
+
+void UpdateManager::selectDirectory()
+{
+    const QString dir = QFileDialog::getExistingDirectory(this);
+
+    if(dir.isEmpty()) return;
+
+    directoryLineEdit->setText(dir);
+}
+
+void UpdateManager::requestUpdate()
+{
+    /* フォームに入力されたURL */
+    const QString urlText = urlLineEdit->text().trimmed();
+    if(urlText.isEmpty()) return;
+
+    /* URL ダウンロードリンク */
+    const QUrl url = QUrl::fromUserInput(urlText);
+    if(!url.isValid()) return;
+
+    /* ダウンロードリンク先のzipファイルのファイル名 */
+    QString fileName = url.fileName();
+    if(fileName.isEmpty()) fileName = defaultFileName;
+
+    /* ダウンロードするディレクトリとダウンロードするファイル名 */
+    QDir dlDirectory(directoryLineEdit->text().trimmed());
+    if(dlDirectory.exists())
+    {
+        newParentFolder = dlDirectory.path();
+
+        file = std::make_unique<QFile>(newParentFolder + '/' + fileName);
+
+        if(!file->open(QIODevice::WriteOnly)) return;
+    }
+    else
+    {   //指定されたダウンロード先のディレクトリが存在しない
+        return;
+    }
+
+    startDownload(url);
+}
+
+void UpdateManager::startDownload(const QUrl& url)
+{
+    reply.reset(networkAccessManager.get(QNetworkRequest(url)));
+
+    connect(reply.get(), &QNetworkReply::readyRead, this, &UpdateManager::readData);
+    connect(reply.get(), &QNetworkReply::finished, this, &UpdateManager::updateFiles);
+
+    ProgressDialog *progressDialog = new ProgressDialog(url, this);
+    progressDialog->setAttribute(Qt::WA_DeleteOnClose);
+    connect(reply.get(), &QNetworkReply::downloadProgress, progressDialog, &ProgressDialog::replyProgress);
+    connect(reply.get(), &QNetworkReply::finished, progressDialog, &ProgressDialog::deleteLater);
+    connect(progressDialog, &ProgressDialog::canceled, this, &UpdateManager::cancelUpdate);
+
+    progressDialog->show();
+    updateButton->setEnabled(false);
+}
+
+void UpdateManager::readData()
+{
+    if(file)
+        file->write(reply->readAll());
+}
+
+void UpdateManager::updateFiles()
+{
+    const QString currentDir = QDir::currentPath();
+
+    file->close();
+
+    //unzipProcess->execute(currentDir + "/release/" + "unzip.exe",
+    //                      QStringList() << "E:/master.zip"
+    //                                    << "-d"
+    //                                    << appParentPath);
+
+    //QDir dir(directoryLineEdit->text());
+    //dir.mkdir("GnuplotEditor-updated");
+
+    connect(unzipProcess, &QProcess::readyReadStandardOutput, [this](){
+        qDebug() <<  unzipProcess->readAllStandardError();
+    });
+    connect(unzipProcess, &QProcess::readyReadStandardError, [this](){
+        qDebug() << unzipProcess->readAllStandardError();
+    });
+
+    qDebug() << file->fileName();
+    qDebug() << newParentFolder;
+
+    unzipProcess->execute(currentDir + "/release/" + "unzip.exe",
+                          QStringList() << "-o"               //上書き確認なし
+                                        << file->fileName()   //解凍するzipファイル名
+                                        << "-d"               //ディレクトリ指定
+                                        << newParentFolder);  //展開するディレクトリ
+
+    file->remove();
+}
+
+void UpdateManager::cancelUpdate()
+{
+    reply->abort();
+    messageLabel->setText("Download canceled");
+    updateButton->setEnabled(true);
+    file->remove();
+}
+
+
