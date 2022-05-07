9e6f4eaf8c5881c60d69d1cafd935240612a10ce
nabla27 <nabla3.1415@gmail.com>
2022-05-07 16:09:06 +0900

    enable to download zip file and unzip the file.

--- src/updatemanager.h
+++ src/updatemanager.h
@@ -5,6 +5,26 @@
 #include <QLineEdit>
 #include <QLabel>
 
+#include <QUrl>
+#include <QNetworkAccessManager>
+#include <QProgressDialog>
+#include <QFile>
+
+#include <memory>
+
+#include <QProcess>
+
+class ProgressDialog : public QProgressDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ProgressDialog(const QUrl& url, QWidget *parent = nullptr);
+
+public slots:
+    void replyProgress(qint64 bytesRead, qint64 totalBytes);
+};
+
 
 class UpdateManager : public QDialog
 {
@@ -16,13 +36,35 @@ public:
     void startRequest(const QUrl& requestedUrl);
 
 private slots:
-
+    void selectDirectory();
+    void requestUpdate();
+    void readData();
+    void updateFiles();
+    void cancelUpdate();
 
 private:
-    static const QString downloadUrl;
+    void startDownload(const QUrl& url);
+
+private:
+    static const QString defaultFileName;     //ダウンロードするzipファイルの名前
+    static const QString downloadUrl;         //ダウンロード先のURL
+
     QLineEdit *urlLineEdit;
+    QLineEdit *directoryLineEdit;
     QLineEdit *versionLineEdit;
     QLabel *messageLabel;
+    QPushButton *updateButton;
+    QPushButton *quitButton;
+
+    QString newParentFolder;
+    QString oldFolderPath;
+
+    QNetworkAccessManager networkAccessManager;
+    QScopedPointer<QNetworkReply, QScopedPointerDeleteLater> reply;
+    std::unique_ptr<QFile> file;
+    bool httpsRequestAborted;
+
+    QProcess *unzipProcess;
 };
 
 #endif // UPDATEMANAGER_H
