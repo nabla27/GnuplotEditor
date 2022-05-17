82f74c4f2ac2b525616f46a2f476ab5ac2028a28
Nabla <63175080+nabla27@users.noreply.github.com>
2022-05-17 17:06:32 +0900

    enable to display template scripts on TemplateCustomWidget.

--- src/templatecustomwidget.cpp
+++ src/templatecustomwidget.cpp
@@ -4,12 +4,14 @@
 #include <QStyle>
 #include <QDir>
 #include "utility.h"
+#include "iofile.h"
 
 TemplateCustomWidget::TemplateCustomWidget(QWidget *parent)
     : QWidget(parent)
     , templateScriptTreeArea(new QScrollArea(this))
     , templateScriptTree(new QWidget(this))
     , templateScriptTreeLayout(new QVBoxLayout(templateScriptTree))
+    , editorPanel(new TemplateEditorPanel(this))
     , editor(new TextEdit(this))
 {
     setWindowFlag(Qt::WindowType::Window, true);
@@ -26,12 +28,14 @@ TemplateCustomWidget::TemplateCustomWidget(QWidget *parent)
     templateListLayout->addWidget(templateScriptTreeArea);
     templateScriptTreeArea->setWidget(templateScriptTree);
     templateScriptTree->setLayout(templateScriptTreeLayout);
+    displayLayout->addWidget(editorPanel);
     displayLayout->addWidget(editor);
 
     templateScriptTreeArea->setLayout(new QVBoxLayout);
     templateScriptTreeArea->setMaximumWidth(200);
     templateScriptTreeArea->setWidgetResizable(true);
     templateScriptTreeLayout->setAlignment(Qt::AlignmentFlag::AlignTop);
+    editor->setReadOnly(true);
 
     setContentsMargins(0, 0, 0, 0);
     templateListLayout->setContentsMargins(0, 0, 0, 0);
@@ -55,14 +59,54 @@ void TemplateCustomWidget::setupTemplateList()
     QDir templateFolderDir(templateFolderPath);
     if(!templateFolderDir.exists()) templateFolderDir.mkdir(templateFolderPath);
 
-
     const QStringList fileList = templateFolderDir.entryList(QStringList() << "*.txt");
     for(const QString& fileName : fileList)
     {
         const QString name = fileName.first(fileName.lastIndexOf('.'));
-        templateScriptTreeLayout->addWidget(new TemplateItemWidget(name, templateScriptTree));
+        TemplateItemWidget *item = new TemplateItemWidget(name, templateScriptTree);
+        templateScriptTreeLayout->addWidget(item);
+
+        connect(item, &TemplateItemWidget::templateSelected, this, &TemplateCustomWidget::setTemplate);
     }
 }
+
+void TemplateCustomWidget::setTemplate(const QString& templateName)
+{
+    const QString templateFilePath = templateFolderPath + "/" + templateName + ".txt";
+
+    QFile file(templateFilePath);
+
+    if(!file.exists())
+    {
+        editor->setPlainText(templateFilePath + " was not found.");
+        return;
+    }
+
+    bool ok = false;
+    const QString script = readFileTxt(templateFilePath, &ok);
+
+    if(!ok)
+    {
+        editor->setPlainText("Could not read a file " + templateFilePath + "\n\n" + script);
+        return;
+    }
+
+    editorPanel->setTemplateName(templateName);
+    editor->setPlainText(script);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 TemplateItemWidget::TemplateItemWidget(const QString& name, QWidget *parent)
     : QWidget(parent)
@@ -95,7 +139,47 @@ TemplateItemWidget::TemplateItemWidget(const QString& name, QWidget *parent)
     buttonLabel->setIndent(10); //ボタン左先頭部分の余白
 
     scriptNameButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    connect(scriptNameButton, &QPushButton::released, this, &TemplateItemWidget::emitSetEditorSignals);
 }
+
+void TemplateItemWidget::emitSetEditorSignals()
+{
+    emit templateSelected(buttonLabel->text());
+}
+
+
+
+
+
+
+
+
+TemplateEditorPanel::TemplateEditorPanel(QWidget *parent)
+    : QWidget(parent)
+    , templateNameEdit(new QLineEdit(this))
+{
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+
+    setLayout(hLayout);
+    hLayout->addWidget(templateNameEdit);
+
+    templateNameEdit->setReadOnly(true);
+
+    hLayout->setAlignment(Qt::AlignmentFlag::AlignLeft);
+    hLayout->setSpacing(0);
+    hLayout->setContentsMargins(0, 0, 0, 0);
+    setContentsMargins(0, 0, 0, 0);
+}
+
+void TemplateEditorPanel::setTemplateName(const QString &templateName)
+{
+    templateNameEdit->setText(templateName);
+}
+
+
+
+
 
 
 
