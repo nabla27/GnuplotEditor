378742e21630a233dcf23eb032b392b123a8359e
Nabla <63175080+nabla27@users.noreply.github.com>
2022-11-24 00:51:47 +0900

    add the TableCellSettingWidget to tableArea.

--- src/tablesettingwidget.cpp
+++ src/tablesettingwidget.cpp
@@ -26,6 +26,7 @@ TableArea::TableArea(QWidget *parent, QStackedWidget *sheetStack)
     , settingScrollArea(new QScrollArea(this))
     , scrollContentsVLayout(new QVBoxLayout)
     , tableEditSettingWidget(new TableEditSettingWidget(this))
+    , tableCellSettingWidget(new TableCellSettingWidget(this))
     , sheetStack(sheetStack)
 {
     QVBoxLayout *vLayout = new QVBoxLayout(this);
@@ -38,9 +39,10 @@ TableArea::TableArea(QWidget *parent, QStackedWidget *sheetStack)
     scrollContents->setLayout(scrollContentsVLayout);
 
     scrollContentsVLayout->addWidget(tableEditSettingWidget);
+    scrollContentsVLayout->addWidget(tableCellSettingWidget);
 
     vLayout->setSpacing(0);
-    scrollContentsVLayout->setSpacing(0);
+    scrollContentsVLayout->setSpacing(1);
     vLayout->setContentsMargins(0, 0, 0, 0);
     settingScrollArea->setContentsMargins(0, 0, 0, 0);
     scrollContents->setContentsMargins(0, 0, 0, 0);
@@ -92,6 +94,30 @@ void TableArea::setupSheetWidget(GnuplotTable *table)
             this, &TableArea::setCellColor);
     connect(tableEditSettingWidget, &TableEditSettingWidget::cellTextColorSet,
             this, &TableArea::setCellTextColor);
+
+    /* tableCellSetting -> table */
+    connect(tableCellSettingWidget, &TableCellSettingWidget::textAlignmentSet,
+            this, &TableArea::setTextAlignment);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::reverseRowRequested,
+            this, &TableArea::reverseRow);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::reverseColRequested,
+            this, &TableArea::reverseCol);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::transposeCellRequested,
+            this, &TableArea::transposeCell);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::clearCellTextRequested,
+            this, &TableArea::clearCellText);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::mergeCellsRequested,
+            this, &TableArea::mergeSelectedCells);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::splitCellsRequested,
+            this, &TableArea::splitSelectedCells);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::insertRowAboveRequested,
+            this, &TableArea::insertRowAbove);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::insertRowBelowRequested,
+            this, &TableArea::insertRowBelow);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::insertColLeftRequested,
+            this, &TableArea::insertColLeft);
+    connect(tableCellSettingWidget, &TableCellSettingWidget::insertColRightRequested,
+            this, &TableArea::insertColRight);
 }
 
 void TableArea::setCurrentSheet(int currentIndex)
@@ -197,6 +223,82 @@ void TableArea::setCellTextColor(const QColor& color)
     }
 }
 
+void TableArea::setTextAlignment(const Qt::AlignmentFlag &flag)
+{
+    if(currentTable)
+    {
+        currentTable->setSelectedTextAlignment(flag);
+    }
+}
+
+void TableArea::reverseRow()
+{
+    if(currentTable)
+    {
+        currentTable->reverseRow();
+    }
+}
+
+void TableArea::reverseCol()
+{
+    if(currentTable)
+    {
+        currentTable->reverseCol();
+    }
+}
+
+void TableArea::transposeCell()
+{
+    if(currentTable)
+    {
+        currentTable->transposeCell();
+    }
+}
+
+void TableArea::clearCellText()
+{
+    if(currentTable)
+    {
+        currentTable->clearCell();
+    }
+}
+
+void TableArea::mergeSelectedCells()
+{
+    if(currentTable)
+    {
+        currentTable->mergeSelectedCells();
+    }
+}
+
+void TableArea::splitSelectedCells()
+{
+    if(currentTable)
+    {
+        currentTable->splitSelectedCells();
+    }
+}
+
+void TableArea::insertRowAbove()
+{
+    if(currentTable) currentTable->insertRowUp();
+}
+
+void TableArea::insertRowBelow()
+{
+    if(currentTable) currentTable->insertRowDown();
+}
+
+void TableArea::insertColLeft()
+{
+    if(currentTable) currentTable->insertColLeft();
+}
+
+void TableArea::insertColRight()
+{
+    if(currentTable) currentTable->insertColRight();
+}
+
 
 
 
@@ -212,11 +314,8 @@ TableEditSettingWidget::TableEditSettingWidget(QWidget *parent)
     , cellColorDialog(new QColorDialog(Qt::white, this))
     , textColorDialog(new QColorDialog(Qt::black, this))
 {
-    QVBoxLayout *vLayout = new QVBoxLayout(this);
-    QHBoxLayout *topHLayout = new QHBoxLayout;
-
-    setLayout(vLayout);
-    vLayout->addLayout(topHLayout);
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    setLayout(hLayout);
 
     mlayout::IconLabel *pasteButton = new mlayout::IconLabel(this);
     mlayout::IconLabel *copyButton = new mlayout::IconLabel(this);
@@ -227,23 +326,23 @@ TableEditSettingWidget::TableEditSettingWidget(QWidget *parent)
     mlayout::IconLabel *cellColorExpand = new mlayout::IconLabel(this);
     mlayout::IconLabel *textColorExpand = new mlayout::IconLabel(this);
 
-    topHLayout->addWidget(new QLabel(" Row "));
-    topHLayout->addWidget(rowCountSpinBox);
-    topHLayout->addWidget(new QLabel(" Col "));
-    topHLayout->addWidget(colCountSpinBox);
-    topHLayout->addWidget(pasteButton);
-    topHLayout->addWidget(copyButton);
-    topHLayout->addWidget(cutButton);
-    topHLayout->addWidget(fontComboBox);
-    topHLayout->addWidget(textSizeSpinBox);
-    topHLayout->addWidget(boldButton);
-    topHLayout->addWidget(italicButton);
-    topHLayout->addWidget(underlineButton);
-    topHLayout->addWidget(cellColorButton);
-    topHLayout->addWidget(cellColorExpand);
-    topHLayout->addWidget(textColorButton);
-    topHLayout->addWidget(textColorExpand);
-    topHLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred));
+    hLayout->addWidget(new QLabel(" Row "));
+    hLayout->addWidget(rowCountSpinBox);
+    hLayout->addWidget(new QLabel(" Col "));
+    hLayout->addWidget(colCountSpinBox);
+    hLayout->addWidget(pasteButton);
+    hLayout->addWidget(copyButton);
+    hLayout->addWidget(cutButton);
+    hLayout->addWidget(fontComboBox);
+    hLayout->addWidget(textSizeSpinBox);
+    hLayout->addWidget(boldButton);
+    hLayout->addWidget(italicButton);
+    hLayout->addWidget(underlineButton);
+    hLayout->addWidget(cellColorButton);
+    hLayout->addWidget(cellColorExpand);
+    hLayout->addWidget(textColorButton);
+    hLayout->addWidget(textColorExpand);
+    hLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred));
 
     static constexpr int iconSize = 20;
 
@@ -288,9 +387,8 @@ TableEditSettingWidget::TableEditSettingWidget(QWidget *parent)
     textColorButton->setToolTip("text color");
     textColorExpand->setToolTip("text color");
 
-    vLayout->setContentsMargins(0, 0, 0, 0);
-    topHLayout->setContentsMargins(0, 0, 0, 0);
-    topHLayout->setSpacing(0);
+    hLayout->setContentsMargins(0, 0, 0, 0);
+    hLayout->setSpacing(1);
 
     textSizeSpinBox->setValue(9);
     cellColorButton->setPaintFrameColor(Qt::white);
@@ -358,6 +456,165 @@ void TableEditSettingWidget::setColCountSpinBox(const int, const int colCount)
     colCountSpinBox->setValue(colCount);
 }
 
+
+
+
+
+
+
+
+
+
+
+TableCellSettingWidget::TableCellSettingWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    setLayout(hLayout);
+    hLayout->setSpacing(1);
+    hLayout->setContentsMargins(0, 0, 0, 0);
+
+    mlayout::IconLabel *alignLeftButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignHCenterButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignRightButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignJustButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignTopButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignVCenterButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignBottomButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *alignBaselineButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *reverseRowButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *reverseColButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *transposeButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *clearTextButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *mergeCellsButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *splitCellsButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *insertRowAboveButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *insertRowBelowButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *insertColLeftButton = new mlayout::IconLabel(this);
+    mlayout::IconLabel *insertColRightButton = new mlayout::IconLabel(this);
+
+    hLayout->addWidget(alignLeftButton);
+    hLayout->addWidget(alignHCenterButton);
+    hLayout->addWidget(alignRightButton);
+    hLayout->addWidget(alignJustButton);
+    hLayout->addWidget(new mlayout::SeparatorLineWidget(this, Qt::Orientation::Vertical, 1, Qt::lightGray, 0));
+    hLayout->addWidget(alignTopButton);
+    hLayout->addWidget(alignVCenterButton);
+    hLayout->addWidget(alignBottomButton);
+    hLayout->addWidget(alignBaselineButton);
+    hLayout->addWidget(new mlayout::SeparatorLineWidget(this, Qt::Orientation::Vertical, 1, Qt::lightGray, 0));
+    hLayout->addWidget(reverseRowButton);
+    hLayout->addWidget(reverseColButton);
+    hLayout->addWidget(transposeButton);
+    hLayout->addWidget(new mlayout::SeparatorLineWidget(this, Qt::Orientation::Vertical, 1, Qt::lightGray, 0));
+    hLayout->addWidget(clearTextButton);
+    hLayout->addWidget(mergeCellsButton);
+    hLayout->addWidget(splitCellsButton);
+    hLayout->addWidget(new mlayout::SeparatorLineWidget(this, Qt::Orientation::Vertical, 1, Qt::lightGray, 0));
+    hLayout->addWidget(insertRowAboveButton);
+    hLayout->addWidget(insertRowBelowButton);
+    hLayout->addWidget(insertColLeftButton);
+    hLayout->addWidget(insertColRightButton);
+    hLayout->addItem(new QSpacerItem(0, 0, QSizePolicy::Expanding, QSizePolicy::Preferred));
+
+    static constexpr int iconSize = 20;
+
+    alignLeftButton->setPixmap(QPixmap(":/icon/edit_alignleft").scaled(iconSize, iconSize));
+    alignHCenterButton->setPixmap(QPixmap(":/icon/edit_alignhcenter").scaled(iconSize, iconSize));
+    alignRightButton->setPixmap(QPixmap(":/icon/edit_alignright").scaled(iconSize, iconSize));
+    alignJustButton->setPixmap(QPixmap(":/icon/edit_alignjust").scaled(iconSize, iconSize));
+    alignTopButton->setPixmap(QPixmap(":/icon/edit_aligntop").scaled(iconSize, iconSize));
+    alignVCenterButton->setPixmap(QPixmap(":/icon/edit_alignvcenter").scaled(iconSize, iconSize));
+    alignBottomButton->setPixmap(QPixmap(":/icon/edit_alignbottom").scaled(iconSize, iconSize));
+    alignBaselineButton->setPixmap(QPixmap(":/icon/edit_alignbaseline").scaled(iconSize, iconSize));
+    reverseRowButton->setPixmap(QPixmap(":/icon/edit_reverserow").scaled(iconSize, iconSize));
+    reverseColButton->setPixmap(QPixmap(":/icon/edit_reversecol").scaled(iconSize, iconSize));
+    transposeButton->setPixmap(QPixmap(":/icon/edit_reverserow").transformed(QTransform().rotate(45)).scaled(iconSize, iconSize));
+    clearTextButton->setPixmap(QPixmap(":/icon/edit_clear").scaled(iconSize, iconSize));
+    mergeCellsButton->setPixmap(QPixmap(":/icon/edit_mergecells").scaled(iconSize, iconSize));
+    splitCellsButton->setPixmap(QPixmap(":/icon/edit_splitcells").scaled(iconSize, iconSize));
+    insertRowAboveButton->setPixmap(QPixmap(":/icon/edit_insertrowabove").scaled(iconSize, iconSize));
+    insertRowBelowButton->setPixmap(QPixmap(":/icon/edit_insertrowbelow").scaled(iconSize, iconSize));
+    insertColLeftButton->setPixmap(QPixmap(":/icon/edit_insertcolleft").scaled(iconSize, iconSize));
+    insertColRightButton->setPixmap(QPixmap(":/icon/edit_insertcolright").scaled(iconSize, iconSize));
+
+    QPalette buttonHoveredPalette(alignLeftButton->foregroundRole(), Qt::lightGray);
+    alignLeftButton->setHoveredPalette(buttonHoveredPalette);
+    alignHCenterButton->setHoveredPalette(buttonHoveredPalette);
+    alignRightButton->setHoveredPalette(buttonHoveredPalette);
+    alignJustButton->setHoveredPalette(buttonHoveredPalette);
+    alignTopButton->setHoveredPalette(buttonHoveredPalette);
+    alignVCenterButton->setHoveredPalette(buttonHoveredPalette);
+    alignBottomButton->setHoveredPalette(buttonHoveredPalette);
+    alignBaselineButton->setHoveredPalette(buttonHoveredPalette);
+    reverseRowButton->setHoveredPalette(buttonHoveredPalette);
+    reverseColButton->setHoveredPalette(buttonHoveredPalette);
+    transposeButton->setHoveredPalette(buttonHoveredPalette);;
+    clearTextButton->setHoveredPalette(buttonHoveredPalette);
+    mergeCellsButton->setHoveredPalette(buttonHoveredPalette);
+    splitCellsButton->setHoveredPalette(buttonHoveredPalette);
+    insertRowAboveButton->setHoveredPalette(buttonHoveredPalette);
+    insertRowBelowButton->setHoveredPalette(buttonHoveredPalette);
+    insertColLeftButton->setHoveredPalette(buttonHoveredPalette);
+    insertColRightButton->setHoveredPalette(buttonHoveredPalette);
+
+    alignLeftButton->setAutoFillBackground(true);
+    alignHCenterButton->setAutoFillBackground(true);
+    alignRightButton->setAutoFillBackground(true);
+    alignJustButton->setAutoFillBackground(true);
+    alignTopButton->setAutoFillBackground(true);
+    alignVCenterButton->setAutoFillBackground(true);
+    alignBottomButton->setAutoFillBackground(true);
+    alignBaselineButton->setAutoFillBackground(true);
+    reverseRowButton->setAutoFillBackground(true);
+    reverseColButton->setAutoFillBackground(true);
+    transposeButton->setAutoFillBackground(true);
+    clearTextButton->setAutoFillBackground(true);
+    mergeCellsButton->setAutoFillBackground(true);
+    splitCellsButton->setAutoFillBackground(true);
+    insertRowAboveButton->setAutoFillBackground(true);
+    insertRowBelowButton->setAutoFillBackground(true);
+    insertColLeftButton->setAutoFillBackground(true);
+    insertColRightButton->setAutoFillBackground(true);
+
+    alignLeftButton->setToolTip("Aligns with the left edge.");
+    alignHCenterButton->setToolTip("Centers horizontally in the available space.");
+    alignRightButton->setToolTip("Aligns with the right edge.");
+    alignJustButton->setToolTip("Justifies the text in the available space.");
+    alignTopButton->setToolTip("Aligns with the top.");
+    alignVCenterButton->setToolTip("Aligns with the bottom.");
+    alignBottomButton->setToolTip("Centers vertically in the available space.");
+    alignBaselineButton->setToolTip("Aligns with the baseline.");
+    reverseRowButton->setToolTip("reverse row");
+    reverseColButton->setToolTip("reverse column");
+    transposeButton->setToolTip("transpose");
+    clearTextButton->setToolTip("clear text");
+    mergeCellsButton->setToolTip("merge cells");
+    splitCellsButton->setToolTip("split cells");
+    insertRowAboveButton->setToolTip("insert row above");
+    insertRowBelowButton->setToolTip("insert row below");
+    insertColLeftButton->setToolTip("insert column left");
+    insertColRightButton->setToolTip("insert column right");
+
+    connect(alignLeftButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignLeft); });
+    connect(alignHCenterButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignHCenter); });
+    connect(alignRightButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignRight); });
+    connect(alignJustButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignJustify); });
+    connect(alignTopButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignTop); });
+    connect(alignVCenterButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignVCenter); });
+    connect(alignBottomButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignBottom); });
+    connect(alignBaselineButton, &mlayout::IconLabel::released, [this](){ emit textAlignmentSet(Qt::AlignBaseline); });
+    connect(reverseRowButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::reverseRowRequested);
+    connect(reverseColButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::reverseColRequested);
+    connect(transposeButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::transposeCellRequested);
+    connect(clearTextButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::clearCellTextRequested);
+    connect(mergeCellsButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::mergeCellsRequested);
+    connect(splitCellsButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::splitCellsRequested);
+    connect(insertRowAboveButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::insertRowAboveRequested);
+    connect(insertRowBelowButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::insertRowBelowRequested);
+    connect(insertColLeftButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::insertColLeftRequested);
+    connect(insertColRightButton, &mlayout::IconLabel::released, this, &TableCellSettingWidget::insertColRightRequested);
+}
 
 
 
